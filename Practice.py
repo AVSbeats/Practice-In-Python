41d6bf097b82d9b402d5e021f60847dbc11f5888
Abraham Vallejos-Soto <125098951+AVSbeats@users.noreply.github.com>
2023-05-03 16:42:21 -0400

    First to main

---
+++ Practice.py
@@ -0,0 +1,8 @@
+first_Name = "Abraham"
+last_Name = "Vallejos-Soto"
+print("Welcome: "+ first_Name + " " + last_Name)
+
+birthday_Year = int(input("What is your year of birth? "))
+age = 2023 - birthday_Year
+print("Your age in 2023 is ")
+print(age)
